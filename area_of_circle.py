c1f2f6803bd56525dcc77a9a5a47b9cc18bc5c03
Subash Lama Tamang <130370071+frgnc-subash@users.noreply.github.com>
2024-11-15 13:51:25 +0545

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,5 @@
+r = int(input("Input the radius of circle in cm: "))
+area_circle = 3.14*r*r
+print(f"The area of cicle is {area_circle} cm square.")
+
+
---
+++ area_of_rectangle.py
@@ -0,0 +1,8 @@
+print("A python program to find the area of rectangle.")
+#Enter length and breadth of rectangle
+l = float(input("Input the length of the rectangle in cm:"))
+b = float(input("Input the breadth of the rectangle in cm:"))
+#Use the formula to find the area of rectangle
+area_rect = l*b
+print(f"The area of rectangle is {area_rect} cm square.")
+
