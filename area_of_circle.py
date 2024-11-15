2066cb238ed154fc3eab90f798d46d555ddedaa3
Subash Lama Tamang <130370071+frgnc-subash@users.noreply.github.com>
2024-11-15 13:56:39 +0545

    area_of_circle.py

--- area_of_circle.py
+++ area_of_circle.py
@@ -1,4 +1,7 @@
+print("A python program to find the area of a circle.")
+#Enter the radius of the circle.
 r = int(input("Input the radius of circle in cm: "))
+#Using the formula to find the area of circle.
 area_circle = 3.14*r*r
 print(f"The area of cicle is {area_circle} cm square.")
 
