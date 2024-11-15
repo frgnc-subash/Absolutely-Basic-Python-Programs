df370ffa240eac74bab8ae7265a14f3c98f1ce79
Subash Lama Tamang <130370071+frgnc-subash@users.noreply.github.com>
2024-11-15 14:05:16 +0545

    arithmatic_operators.py

--- arithmatic_operations.py
+++ arithmatic_operations.py
@@ -1,12 +1,11 @@
 #The basic arithmatic functions in Python.
-print(10/3) # division
-print(10//3) # division
-print(10%3) #  moduluos
-print(10**3) # 10 to the power 3
-
-x = 30
-x = x + 10 # Increment of a number
-print(x) 
+print(10+3) #addition
+print(10-3) #subtraction
+print(10*3) #multiplication
+print(10/3) #integral division
+print(10//3) #floor division
+print(10%3) #modulo operator
+print(10**3) # 10 to the power 3 
 
 y = 50
 y -= 10 # Decrement of a number
