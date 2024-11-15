30dffbe5295ae52ca20e90e58d0f30220e75841e
Subash Lama Tamang <130370071+frgnc-subash@users.noreply.github.com>
2024-11-15 14:00:17 +0545

    Add files via upload

---
+++ arithmatic_operations.py
@@ -0,0 +1,21 @@
+#The basic arithmatic functions in Python.
+print(10/3) # division
+print(10//3) # division
+print(10%3) #  moduluos
+print(10**3) # 10 to the power 3
+
+x = 30
+x = x + 10 # Increment of a number
+print(x) 
+
+y = 50
+y -= 10 # Decrement of a number
+print(y)
+
+a = 50  
+a += 10 # Increment of a number
+print(a)
+
+z = (10 + 3) * 4 ** 3 #Python follows BODMAS system 
+print(z) 
+
